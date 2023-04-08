openglfreak/wine

dsound: Fake success for EAX Set Buffer/ListenerProperties
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=50551

dlls/dsound/eax.c
@@ -1082,11 +1082,11 @@ HRESULT WINAPI EAX_Set(IDirectSoundBufferImpl *buf, REFGUID guidPropSet,
 
         return S_OK;
     } else if (IsEqualGUID(&DSPROPSETID_EAX20_ListenerProperties, guidPropSet)) {
-        FIXME("Unsupported DSPROPSETID_EAX20_ListenerProperties: %d\n", dwPropID);
-        return E_PROP_ID_UNSUPPORTED;
+        FIXME("Unsupported DSPROPSETID_EAX20_ListenerProperties: %d - Faking Success\n", dwPropID);
+        return S_OK;
     } else if (IsEqualGUID(&DSPROPSETID_EAX20_BufferProperties, guidPropSet)) {
-        FIXME("Unsupported DSPROPSETID_EAX20_BufferProperties: %d\n", dwPropID);
-        return E_PROP_ID_UNSUPPORTED;
+        FIXME("Unsupported DSPROPSETID_EAX20_BufferProperties: %d - Faking Success\n", dwPropID);
+        return S_OK;
     }
 
     FIXME("(%p,%s,%d,%p,%d,%p,%d)\n",

polkaulfield/wine-lol-src

dlls/dsound/eax.c
@@ -1082,11 +1082,11 @@ HRESULT WINAPI EAX_Set(IDirectSoundBufferImpl *buf, REFGUID guidPropSet,
 
         return S_OK;
     } else if (IsEqualGUID(&DSPROPSETID_EAX20_ListenerProperties, guidPropSet)) {
-        FIXME("Unsupported DSPROPSETID_EAX20_ListenerProperties: %ld\n", dwPropID);
-        return E_PROP_ID_UNSUPPORTED;
+        FIXME("Unsupported DSPROPSETID_EAX20_ListenerProperties: %ld - Faking Success\n", dwPropID);
+        return S_OK;
     } else if (IsEqualGUID(&DSPROPSETID_EAX20_BufferProperties, guidPropSet)) {
-        FIXME("Unsupported DSPROPSETID_EAX20_BufferProperties: %ld\n", dwPropID);
-        return E_PROP_ID_UNSUPPORTED;
+        FIXME("Unsupported DSPROPSETID_EAX20_BufferProperties: %ld - Faking Success\n", dwPropID);
+        return S_OK;
     }
 
     FIXME("(%p,%s,%ld,%p,%ld,%p,%ld)\n",