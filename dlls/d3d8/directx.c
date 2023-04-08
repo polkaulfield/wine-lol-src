tannisroot/wine-1

wined3d: Use UBO for vertex shader float constants if supported.
Signed-off-by: Paul Gofman <gofmanp@gmail.com>

dlls/d3d8/directx.c
@@ -416,7 +416,7 @@ BOOL d3d8_init(struct d3d8 *d3d8)
     DWORD flags = WINED3D_LEGACY_DEPTH_BIAS | WINED3D_VIDMEM_ACCOUNTING
             | WINED3D_HANDLE_RESTORE | WINED3D_PIXEL_CENTER_INTEGER
             | WINED3D_LEGACY_UNBOUND_RESOURCE_COLOR | WINED3D_NO_PRIMITIVE_RESTART
-            | WINED3D_LEGACY_CUBEMAP_FILTERING;
+            | WINED3D_LEGACY_CUBEMAP_FILTERING | WINED3D_LEGACY_SHADER_CONSTANTS;
 
     d3d8->IDirect3D8_iface.lpVtbl = &d3d8_vtbl;
     d3d8->refcount = 1;

include/wine/wined3d.h
@@ -1334,6 +1334,7 @@ enum wined3d_shader_type
 #define WINED3D_NO_PRIMITIVE_RESTART                            0x00000800
 #define WINED3D_LEGACY_CUBEMAP_FILTERING                        0x00001000
 #define WINED3D_NORMALIZED_DEPTH_BIAS                           0x00002000
+#define WINED3D_LEGACY_SHADER_CONSTANTS                         0x00004000
 
 #define WINED3D_RESZ_CODE                                       0x7fa05000
 

polkaulfield/wine-lol-src

dlls/d3d8/directx.c
@@ -476,7 +476,7 @@ BOOL d3d8_init(struct d3d8 *d3d8)
     DWORD flags = WINED3D_LEGACY_DEPTH_BIAS | WINED3D_HANDLE_RESTORE
             | WINED3D_PIXEL_CENTER_INTEGER  | WINED3D_LEGACY_UNBOUND_RESOURCE_COLOR
             | WINED3D_NO_PRIMITIVE_RESTART  | WINED3D_LEGACY_CUBEMAP_FILTERING
-            | WINED3D_NO_DRAW_INDIRECT;
+            | WINED3D_NO_DRAW_INDIRECT  | WINED3D_LEGACY_SHADER_CONSTANTS;
     unsigned int adapter_idx, output_idx, adapter_count, output_count = 0;
     struct wined3d_adapter *wined3d_adapter;
 

include/wine/wined3d.h
@@ -1324,6 +1324,7 @@ enum wined3d_memory_segment_group
 #define WINED3D_LEGACY_CUBEMAP_FILTERING                        0x00001000
 #define WINED3D_NORMALIZED_DEPTH_BIAS                           0x00002000
 #define WINED3D_NO_DRAW_INDIRECT                                0x00004000
+#define WINED3D_LEGACY_SHADER_CONSTANTS                         0x00008000
 
 #define WINED3D_RESZ_CODE                                       0x7fa05000
 
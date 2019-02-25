b2f9e07da1ce9de263d032fe7085d6e9be9a42b4
Paul Gofman <gofmanp@gmail.com>
2019-02-25 13:17:01 +0300

    wined3d: Use UBO for vertex shader float constants if supported.

--- include/wine/wined3d.h
+++ include/wine/wined3d.h
@@ -1324,6 +1324,7 @@ enum wined3d_memory_segment_group
 #define WINED3D_LEGACY_CUBEMAP_FILTERING                        0x00001000
 #define WINED3D_NORMALIZED_DEPTH_BIAS                           0x00002000
 #define WINED3D_NO_DRAW_INDIRECT                                0x00004000
+#define WINED3D_LEGACY_SHADER_CONSTANTS                         0x00008000
 
 #define WINED3D_RESZ_CODE                                       0x7fa05000
 
